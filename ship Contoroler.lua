1f2705aecca7ae1ee1b4d17810c1ce8e0efc725b
SOAU <61126105+SOAU18@users.noreply.github.com>
2024-12-19 09:08:38 +0900

    Add files via upload

--- ship Contoroler.lua
+++ ship Contoroler.lua
@@ -1,6 +1,3 @@
-Count=1
-kp=0.1
-TargetSpeeds={}
 function Pulse(bool)
     local previous_input=false
     return function(bool)
@@ -13,12 +10,17 @@ function Pulse(bool)
         return false 
     end
 end
+
 function Threshold(number,low,max)
     return number>=low and number<=max
 end
+
+Count=6
+TargetSpeeds={-15,-12,-9,-6,-3,0,3,6,9,12,15,18,21,24,27,30,33}
 Axis1_pulse=Pulse()
 UP_pulse=Pulse()
 Down_pulse=Pulse()
+
 function onTick()
     Axis1=input.getNumber(1)
     Axis2=input.getNumber(2)
@@ -29,36 +31,30 @@ function onTick()
     TargetX=input.getNumber(11)
     TargetY=input.getNumber(12)
     Autopilot=input.getBool(1)
---船の方向のコントロール
-    --Axis1が0になったら現在向いている方向を記録
+
     if Axis1_pulse(Threshold(Axis1,-0.1,0.1)) then
         Heading=Compass
     end
-    --オートパイロットがオンならば舵角を目的地の方向に向ける
     if Autopilot then
         Xdifference=TargetX-CurrentX
         Ydifference=TargetY-CurrentY
-        TargetBearing=math.atan(Xdifference,Ydifference)-(math.pi*2)
-        Rudder_angle=((TargetBearing%1-Compass%1+2.5)%1-0.5)*kp
-    --オートパイロットがオフでaxis1が0ならば記録した方向を向き続ける(HDG hold)
+        TargetBearing=math.atan(Xdifference,Ydifference)/(math.pi*2)
+        Rudder_angle=((TargetBearing%1-Compass%1+2.5)%1-0.5)*1
     elseif Threshold(Axis1,-0.1,0.1) then
-        Rudder_angle=((Heading%1-Compass%1+2.5)%1-0.5)*kp
-    --その他の場合はaxis1をそのまま舵に流す
+        Rudder_angle=(Heading-Compass)*2
     else
         Rudder_angle=Axis1
     end
---スピードのコントロール
-    if UP_pulse(Axis2==1) and Count<Max_Value then
+    if UP_pulse(Axis2==1) and Count<17 then
         Count=Count+1
     elseif Down_pulse(Axis2==-1) and Count>1 then
         Count=Count-1
     end
     TargetSpeed=TargetSpeeds[Count]
-    if TargetSpeed<0 then
-        Gear=true
-    end
-    Clutch_Pressure=math.abs((TargetSpeed-Currentspead)*kp)
+    Gear=TargetSpeed<0
+    Clutch_Pressure=(math.abs(TargetSpeed)-Currentspead*1.944)*0.5
     output.setNumber(1,Clutch_Pressure)
     output.setNumber(2,Rudder_angle)
+    output.setNumber(3,TargetSpeed)
     output.setBool(1,Gear)
 end
